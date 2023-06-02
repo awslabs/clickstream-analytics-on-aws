f2bcf708980a56dd945cf53674edafe94c7ab75d
Wei Qiao <wqqiao@amazon.com>
2023-06-02 14:07:26 +0000

    fix: upsert dim users and optimize performance

--- src/analytics/private/sqls/redshift/dim-users.sql
+++ src/analytics/private/sqls/redshift/dim-users.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS {{schema}}.{{table_dim_users}}(
+  event_date DATE,
   event_timestamp BIGINT,
   user_id VARCHAR(255),
   user_properties SUPER,
   user_pseudo_id VARCHAR(255),
   create_date TIMESTAMP default getdate()
-) sortkey(user_pseudo_id, event_timestamp)
+) SORTKEY(user_pseudo_id)
