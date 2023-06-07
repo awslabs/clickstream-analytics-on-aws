1c6ff9f778963e8101f609c1403b0b5078c553ae
Yong Liu <yonmzn@amazon.com>
2023-06-07 09:06:14 +0000

    chore: example build shell

--- examples/custom-plugins/build.sh
+++ examples/custom-plugins/build.sh
@@ -7,12 +7,17 @@ echo $curr_dir
 
 echo "Build custom-enrich ...."
 
-cd $curr_dir/custom-sdk-transformer
+cd $curr_dir/custom-enrich
 
 ./gradlew clean build
 
+ls -l  ./build/libs/*.jar
+
+echo ""
 echo "Build custom-sdk-transformer ...."
 
 cd $curr_dir/custom-sdk-transformer
 
 ./gradlew clean build
+
+ls -l  ./build/libs/*.jar
