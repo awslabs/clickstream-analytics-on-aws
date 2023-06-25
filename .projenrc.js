ce520c5120fa5688c4d8786973b89a29d22fb737
Kane Zhu <843303+zxkane@users.noreply.github.com>
2023-06-25 13:40:51 +0800

    ci: enable github workflow checks

--- .projenrc.js
+++ .projenrc.js
@@ -11,7 +11,7 @@
  *  and limitations under the License.
  */
 
-const { awscdk, gitlab, typescript } = require('projen');
+const { awscdk, github, gitlab, typescript } = require('projen');
 const version = '1.0.0';
 const cdkVersion = '2.81.0';
 
@@ -110,10 +110,11 @@ const devDepsForApiProject = [
   '@types/express@^4.17.16',
   '@types/supertest@^2.0.12',
 ];
+const defaultBranch = 'main';
 const project = new awscdk.AwsCdkTypeScriptApp({
   version,
   cdkVersion,
-  defaultReleaseBranch: 'main',
+  defaultReleaseBranch: defaultBranch,
   name: 'clickstream-analytics-on-aws',
   gitignore: [
     '.idea/',
@@ -173,6 +174,20 @@ const project = new awscdk.AwsCdkTypeScriptApp({
       emitDecoratorMetadata: true,
     },
   },
+  githubOptions: {
+    pullRequestLintOptions: {
+      semanticTitleOptions: {
+        types: [
+          'feat',
+          'fix',
+          'chore',
+          'docs',
+          'ci',
+          'tests',
+        ],
+      },
+    },
+  },
 });
 
 project.eslint?.addRules({
@@ -215,7 +230,7 @@ const apiProject = new typescript.TypeScriptProject({
   outdir: 'src/control-plane/backend/lambda/api/',
   libdir: 'dist/',
   readme: undefined,
-  defaultReleaseBranch: 'main',
+  defaultReleaseBranch: defaultBranch,
   entrypoint: 'index.js',
   parent: project,
   sampleCode: false,
@@ -237,6 +252,9 @@ const apiProject = new typescript.TypeScriptProject({
 apiProject.setScript('dev', 'nodemon --watch \'src\' -e ts --exec \'ts-node\' ./index.ts');
 apiProject.setScript('start', 'node dist/index.js');
 
+project.buildWorkflow.buildTask._env = {
+  NODE_OPTIONS: '--max_old_space_size=6144',
+};
 
 const provisionViperlightScripts = [
   'curl -sL https://deb.nodesource.com/setup_16.x | bash -',
@@ -248,6 +266,7 @@ const provisionViperlightScripts = [
   'curl https://viperlight-scanner.s3.us-east-1.amazonaws.com/latest/viperlight.zip -o viperlight.zip',
   'unzip -q viperlight.zip -d ../viperlight && rm viperlight.zip',
 ];
+
 const gitlabMain = new gitlab.GitlabConfiguration(project,
   {
     workflow: {
---
+++ deployment/test-build-dist-1.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+export SOLUTION_NAME='test-clickstream-001'
+export BUILD_VERSION=ci-latest-001
+
+export BSS_IMAGE_ASSET_REPOSITORY_NAME=clickstream
+export AWS_ASSET_ACCOUNT_ID=555555555555
+export AWS_CN_ASSET_ACCOUNT_ID=444455556666
+export REGIONS='us-west-1,us-west-2'
+export AWS_ASSET_PUBLISH_ROLE='arn:aws:iam::555555555555:role/cross-account-publishing-role'
+export GLOBAL_ASSETS='default/'
+export CN_ASSETS='cn/'
+export TARGET='feature-rel/test'
+export OUT_TAG_IMAGE_SHELL_FILE='test-tag-images.sh'
+
+export SOLUTION_ECR_ACCOUNT=$AWS_ASSET_ACCOUNT_ID
+export SOLUTION_ECR_REPO_NAME=$SOLUTION_NAME
+
+# in gcr pipeline
+export IS_IN_GCR_PIPELINE=1
+export DIST_OUTPUT_BUCKET='aws-gcr-solutions'
+./deployment/build-s3-dist-1.sh solution-bucket $SOLUTION_NAME $BUILD_VERSION
+node ./deployment/test/index.js
