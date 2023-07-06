55b364013560cd88cdc7c75e81001165a51e48c9
Que Mingfei <mingfeiq@amazon.com>
2023-07-06 03:52:20 +0000

    chore: update cfn-nag suppression messages

--- src/control-plane/backend/click-stream-api.ts
+++ src/control-plane/backend/click-stream-api.ts
@@ -313,7 +313,7 @@ export class ClickStreamApiConstruct extends Construct {
     addCfnNagSuppressRules(this.clickStreamApiFunction.node.defaultChild as CfnResource, [
       {
         id: 'W92',
-        reason: 'TODO: will revisit it based on T-shirt size of workload',
+        reason: 'Lambda function is only used by ClickStreamApiFunction for deployment as cloudformation custom resources or per product design, no need to set ReservedConcurrentExecutions',
       },
     ]);
 
@@ -364,7 +364,7 @@ export class ClickStreamApiConstruct extends Construct {
           },
           {
             id: 'W92',
-            reason: 'TODO: will revisit it based on T-shirt size of workload',
+            reason: 'Lambda function is only used by ClickStreamApiFunction for deployment as cloudformation custom resources or per product design, no need to set ReservedConcurrentExecutions',
           },
         ],
       );
