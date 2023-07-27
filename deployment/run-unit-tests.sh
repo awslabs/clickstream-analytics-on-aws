28ae9941888864f09d6a57d395239579ff4eb3dc
Meng Xin Zhu <843303+zxkane@users.noreply.github.com>
2023-07-27 12:49:32 +0800

    chore: include frontend in sonar scan & report (#99)

--- deployment/run-unit-tests.sh
+++ deployment/run-unit-tests.sh
@@ -9,17 +9,23 @@ cp src/common/constant.ts frontend/src/ts/constant-ln.ts
 rm src/control-plane/backend/lambda/api/common/*-ln.ts
 cp src/common/constant.ts src/control-plane/backend/lambda/api/common/constants-ln.ts
 cp src/common/model.ts src/control-plane/backend/lambda/api/common/model-ln.ts
-cp src/common/sdk-client-config.ts src/control-plane/backend/lambda/api/common/sdk-client-config-ln.ts 
+cp src/common/sdk-client-config.ts src/control-plane/backend/lambda/api/common/sdk-client-config-ln.ts
 cp src/common/solution-info.ts src/control-plane/backend/lambda/api/common/solution-info-ln.ts
-rm src/control-plane/backend/lambda/api/middle-ware/authorizer.ts 
-cp src/control-plane/auth/authorizer.ts src/control-plane/backend/lambda/api/middle-ware/authorizer.ts 
+rm src/control-plane/backend/lambda/api/middle-ware/authorizer.ts
+cp src/control-plane/auth/authorizer.ts src/control-plane/backend/lambda/api/middle-ware/authorizer.ts
 
 
 echo "yarn install"
 yarn install --check-files --frozen-lockfile
 
-npx projen 
+npx projen
 
 echo "npx yarn test"
 npx yarn test
-   
+
+export CI=true
+npx yarn install --check-files --frozen-lockfile --cwd frontend
+npx yarn --cwd frontend run test
+
+docker run -i --rm -v `pwd`/src/data-pipeline/spark-etl/:/data --workdir /data \
+  public.ecr.aws/docker/library/gradle:7.6-jdk11 gradle test jacocoAggregatedReport
