2b88e6e242688f1ce155ab4e2186090efa032119
Magic Chen <haiyunc@amazon.com>
2023-10-11 23:06:56 -0500

    chore: optimize regex code in frontend

--- frontend/src/ts/utils.ts
+++ frontend/src/ts/utils.ts
@@ -61,9 +61,11 @@ export const checkStringValidRegex = (str: string, regex: RegExp) => {
   return regex.test(str);
 };
 
+export const EMAIL_PATTERN =
+  '\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*';
 export const validateEmails = (emails: string) => {
   const emailArray = emails.split(',');
-  const regex = /\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/;
+  const regex = new RegExp(EMAIL_PATTERN);
   for (const item of emailArray) {
     const email = item.trim();
     if (!regex.test(email)) {
@@ -82,7 +84,7 @@ export const validateProjectId = (projectId: string) => {
 };
 
 export const validateAppId = (appId: string) => {
-  const regex = /^[a-zA-Z][a-zA-Z0-9_]{0,126}$/;
+  const regex = /^[a-zA-Z]\w{0,126}$/;
   if (!regex.test(appId)) {
     return false;
   }
@@ -90,7 +92,7 @@ export const validateAppId = (appId: string) => {
 };
 
 export const validatePluginName = (name: string) => {
-  const re = /[^0-9a-zA-Z_\- |-]/g;
+  const re = /[^0-9a-zA-Z_\- |]/g;
   if (!re?.test(name)) {
     return true;
   }
@@ -98,7 +100,7 @@ export const validatePluginName = (name: string) => {
 };
 
 export const validatePluginMainFunction = (functionName: string) => {
-  const re = /[^0-9a-zA-Z._\- |-]/g;
+  const re = /[^0-9a-zA-Z._\- |]/g;
   if (!re?.test(functionName)) {
     return true;
   }
@@ -229,7 +231,7 @@ export const reverseFreshnessInHour = (value: number) => {
 };
 
 export const extractAccountIdFromArn = (arn: string) => {
-  const regex = /^arn:aws.*:redshift-serverless:[^:]+:([0-9]{12}):/;
+  const regex = /^arn:aws.*:redshift-serverless:[^:]+:(\d{12}):/;
   const matchResult = arn.match(regex);
   return matchResult ? matchResult[1] : '';
 };
@@ -248,7 +250,7 @@ export const isEmpty = (a: any) => {
 };
 
 export const extractRegionFromCloudWatchArn = (arn: string) => {
-  const regex = /^arn:aws.*:cloudwatch:(\w{2}-\w{1,10}-\d):[0-9]{12}:/;
+  const regex = /^arn:aws.*:cloudwatch:(\w{2}-\w{1,10}-\d):\d{12}:/;
   const matchResult = arn.match(regex);
   return matchResult ? matchResult[1] : '';
 };
