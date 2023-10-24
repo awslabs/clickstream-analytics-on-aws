5c2460638149e1d00c176d6bfc61f829a1730347
Yong <yonmzn@amazon.com>
2023-10-24 03:04:52 +0000

    change event_param_string_value to VARCHAR(4096)

--- src/analytics/private/sqls/redshift/event-parameter.sql
+++ src/analytics/private/sqls/redshift/event-parameter.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS {{schema}}.{{table_event_parameter}}(
     event_param_double_value DOUBLE PRECISION,
     event_param_float_value DOUBLE PRECISION,
     event_param_int_value BIGINT,
-    event_param_string_value VARCHAR(255)    
+    event_param_string_value VARCHAR(4096)    
 ) DISTSTYLE EVEN 
 SORTKEY(event_timestamp, event_name)
