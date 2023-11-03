d4221b7997e5f7e2d9e46828ff63f906460281c5
Magic Chen <haiyunc@amazon.com>
2023-11-03 22:04:42 +0800

    chore: update min and max size

--- src/alb-control-plane-stack.ts
+++ src/alb-control-plane-stack.ts
@@ -156,8 +156,8 @@ export class ApplicationLoadBalancerControlPlaneStack extends Stack {
         platform: Platform.LINUX_AMD64,
         buildArgs: {
           GENERATE_SOURCEMAP: process.env.GENERATE_SOURCEMAP ?? 'false',
-          CHUNK_MIN_SIZE: process.env.CHUNK_MIN_SIZE ?? '102400',
-          CHUNK_MAX_SIZE: process.env.CHUNK_MAX_SIZE ?? '204800',
+          CHUNK_MIN_SIZE: process.env.CHUNK_MIN_SIZE ?? '819200',
+          CHUNK_MAX_SIZE: process.env.CHUNK_MAX_SIZE ?? '1024000',
         },
       },
     });
