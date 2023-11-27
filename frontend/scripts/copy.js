3aaa085a6a6b30a2344b680c19988b8087af28a9
Magic Chen <haiyunc@amazon.com>
2023-11-27 09:47:33 +0800

    chore: update frontend

---
+++ frontend/scripts/copy.js
@@ -0,0 +1,40 @@
+/**
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ *  with the License. A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+async function copyDirectory(src, dest) {
+    try {
+        await fs.promises.mkdir(dest, { recursive: true });
+        const entries = await fs.promises.readdir(src, { withFileTypes: true });
+        
+        for (let entry of entries) {
+            const srcPath = path.join(src, entry.name);
+            const destPath = path.join(dest, entry.name);
+            
+            if (entry.isDirectory()) {
+                await copyDirectory(srcPath, destPath);
+            } else {
+                await fs.promises.copyFile(srcPath, destPath);
+            }
+        }
+    } catch (err) {
+        console.error('error:', err);
+    }
+}
+
+const sourceDir = '../src/base-lib';
+const targetDir = './node_modules/@clickstream/base-lib';
+
+copyDirectory(sourceDir, targetDir);
