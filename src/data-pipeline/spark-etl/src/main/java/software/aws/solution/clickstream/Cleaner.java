ff477c4b02ced194322df2edaf85c2592a5be430
Yong Liu <yonmzn@amazon.com>
2023-12-05 11:53:44 +0800

    remove imports

--- src/data-pipeline/spark-etl/src/main/java/software/aws/solution/clickstream/Cleaner.java
+++ src/data-pipeline/spark-etl/src/main/java/software/aws/solution/clickstream/Cleaner.java
@@ -36,14 +36,12 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
 import static java.util.Objects.requireNonNull;
-import static org.apache.spark.sql.functions.coalesce;
 import static org.apache.spark.sql.functions.col;
 import static org.apache.spark.sql.functions.udf;
 import static org.apache.spark.sql.functions.from_json;
