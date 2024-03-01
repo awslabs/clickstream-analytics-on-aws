182885d682bab11bf7fe4869d722721c7c807918
mingtong <mingtong@amazon.com>
2024-03-01 23:25:51 +0800

    feat: implement heartbeat api in ingestion (#910)

--- test/jestEnv.js
+++ test/jestEnv.js
@@ -60,3 +60,6 @@ process.env.S3_BUCKET = 'test-pipe-line-bucket';
 process.env.S3_PREFIX = 'pipeline-prefix/';
 
 process.env.IS_SKIP_VPC_CONNECTION_FORCE_WAITING = 'true'
+
+// env variables for ingestion stack
+process.env.PING_PATH = '/ping'
