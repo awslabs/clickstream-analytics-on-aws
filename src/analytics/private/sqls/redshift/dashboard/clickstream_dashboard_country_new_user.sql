a6b8642e0307fc37cd62a025754be40fa1af9b4a
liwmin <liwmin@amazon.com>
2024-03-19 19:15:52 +0800

    fix typo

--- src/analytics/private/sqls/redshift/dashboard/clickstream_dashboard_country_new_user.sql
+++ src/analytics/private/sqls/redshift/dashboard/clickstream_dashboard_country_new_user.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS {{dbName}}.{{schema}}.clickstream_dashboard_coutry_new_user (
+CREATE TABLE IF NOT EXISTS {{dbName}}.{{schema}}.clickstream_dashboard_country_new_user (
     event_date date,
     geo_country varchar(255),
     user_count bigint
