ff9dc79c67719a8a01ede94619c3800b2daf6723
liwmin <liwmin@amazon.com>
2024-03-20 17:03:57 +0800

    update dashboard definition

--- src/analytics/private/sqls/redshift/dashboard/clickstream_acquisition_country_new_user.sql
+++ src/analytics/private/sqls/redshift/dashboard/clickstream_acquisition_country_new_user.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS {{database_name}}.{{schema}}.clickstream_acquisition_country_new_user (
     event_date date,
+    platform varchar(255),
     geo_country varchar(255),
     user_count bigint
 )
