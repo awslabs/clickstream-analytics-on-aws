07f4cd52d0c43402b75237bb41f8d537553b5449
Yong Liu <yonmzn@amazon.com>
2024-04-01 11:17:38 +0800

    format sql

--- src/analytics/private/sqls/redshift/user-m-view-v2.sql
+++ src/analytics/private/sqls/redshift/user-m-view-v2.sql
@@ -4,7 +4,7 @@ DISTSTYLE ALL
 SORTKEY(user_pseudo_id)
 AUTO REFRESH NO AS
 SELECT  u.user_pseudo_id
-       ,u.event_timestamp,
+       ,u.event_timestamp
        ,MAX(user_id) user_id
        ,MAX(user_properties) user_properties
        ,MAX(user_properties_json_str) user_properties_json_str
