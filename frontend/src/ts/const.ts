e74a35f27c7fd1a14288ca62d7d2e8f60a3ea9a7
Magic Chen <haiyunc@amazon.com>
2024-04-07 11:14:58 +0800

    chore: redirect the last visited page when user re-login (#1023)

--- frontend/src/ts/const.ts
+++ frontend/src/ts/const.ts
@@ -26,6 +26,7 @@ export const ANALYTICS_NAV_STATUS = 'ClickStreamAnalyticsNavigationStatus';
 export const ANALYTICS_NAV_ITEM = 'ClickStreamAnalyticsNavigationItem';
 export const CLICK_STREAM_USER_DATA = 'ClickStreamAnalyticsUserInformation';
 export const CONFIG_URL = '/aws-exports.json';
+export const LAST_VISIT_URL = 'ClickStreamAnalyticsLastVisitUrl';
 
 export const ALPHABETS = Array.from({ length: 26 }, (_, index) =>
   String.fromCharCode(65 + index)
