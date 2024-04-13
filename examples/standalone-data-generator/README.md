3db4fe92e2d4459e0a43f39d47ef4a6db7d36ee0
mingtong <mingtong@amazon.com>
2024-04-13 21:48:27 +0800

    add performance tool

--- examples/standalone-data-generator/README.md
+++ examples/standalone-data-generator/README.md
@@ -110,3 +110,43 @@ DEFAULT_PRODUCT_COUNT = 2
 ```
 
 Please modify the `enums.py` to define the detail logic for device and user information. 
+
+## Clickstream sample data for performance test
+We have developed a tool for performance tests, which utilizes ECS Fargate to continuously send test data to Clickstream. The number of ECS Service tasks can be increased or decreased to adjust the RPS (Requests per Second).
+
+Follow below step to start your Clickstream performance test
+1. Add configuration file:
+
+Please download your `amplifyconfiguration.json` file from your web console of the solution, then copy it to the root
+folder which at the same level as README file. then you can execute the following command to create and send your sample
+data.
+
+In the `amplifyconfiguration.json` file, we will only parse `appId`, `endpoint` and `isCompressEvents` three parameter
+to run the program.
+
+2. Start performance test:
+Run below script to start performance test
+```
+./create-ecs-performance-test.sh <testName> <taskNumber> <region>
+```
+
+<testName>: the suffix of the ECS cluster name, for example: if <testName> is **MyTest**, you ECS cluster name is **clickstream-sample-data-cluster-MyTest**
+<taskNumber>: the number of your ECS service tasks, you can change it from the ECS console after the cluster created.
+<region>: the region where the ECS cluster created.
+For example: 
+```
+./create-ecs-performance-test.sh MyTest 5 us-east-1
+```
+
+According to our testing, if the ECS cluster of performance tool is created in the same region of the Clickstream solution, a task can generate about 200 RPS.
+A request payload size is ~2KB and contains 10 events.
+
+3. Clean up performance test resources.
+run below script to clean up all resources of performance test tool
+```
+./cleanup-ecs-performance-test.sh <testName> <region>
+```
+
+
+
+
---
+++ examples/standalone-data-generator/create_event_for_performance.py
@@ -0,0 +1,133 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+with the License. A copy of the License is located at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+"""
+import random
+import configure
+import enums as enums
+import send_event_real_time
+import util.util as utils
+from application.AppProvider import AppProvider
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+global_current_time = utils.current_timestamp()
+global_total_events_for_duration = 0
+global_total_users_for_duration = 0
+app_provider = AppProvider()
+left_users = []
+left_events = []
+
+
+def init_all_user():
+    user_list = []
+    for _ in range(configure.ALL_USER_REALTIME_PERFORMANCE):
+        user_list.append(app_provider.get_random_user())
+    return user_list
+
+
+def get_user_event_of_duration(users, start_timestamp):
+    all_events = []
+    end_timestamp = start_timestamp + configure.BATCH_EVENT_DURATION_IN_MINUTES_PERFORMANCE * 60 * 1000
+    global global_total_events_for_duration, global_total_users_for_duration
+    removed_users = []
+    for user in users:
+        events = []
+        session_times = random.choices(configure.SESSION_TIMES)[0]
+        start_time_arr = []
+        for i in range(session_times):
+            session_start_time = random.randint(start_timestamp, end_timestamp)
+            if start_timestamp <= session_start_time <= end_timestamp:
+                start_time_arr.append(session_start_time)
+        start_time_arr = sorted(start_time_arr)
+
+        for i in range(len(start_time_arr)):
+            current_timestamp = start_time_arr[i]
+            user.current_timestamp = current_timestamp
+            app_provider.generate_session_events(user, events)
+        if len(events) > 0:
+            all_events.append(events)
+            user.total_day_events = len(events)
+            user.send_events = 0
+            global_total_events_for_duration += len(events)
+        else:
+            removed_users.append(user)
+    for user in removed_users:
+        users.remove(user)
+    global_total_users_for_duration += len(users)
+    return all_events
+
+
+def send_user_event_of_duration(users, all_events, end_timestamp):
+    stop_time_stamp = end_timestamp + configure.FLUSH_DURATION_PERFORMANCE * 1000
+    while utils.current_timestamp() <= stop_time_stamp:
+        for i in range(len(all_events)):
+            for j in range(0, len(all_events[i]), 10):
+                chunk = all_events[i][j:j + 10]
+                if len(chunk) < 10:
+                    break
+                send_event_real_time.send_events_of_day(users[i], chunk)
+    for i in range(len(all_events)):
+        if len(all_events[i]) > 0:
+            left_events.append(all_events[i])
+            left_users.append(users[i])
+
+
+def create_duration_event(day_users):
+    global global_total_users_for_duration, global_total_events_for_duration
+    global_total_users_for_duration = 0
+    global_total_events_for_duration = 0
+    start_timestamp = utils.current_timestamp()
+    end_timestamp = start_timestamp + configure.BATCH_EVENT_DURATION_IN_MINUTES_PERFORMANCE * 60 * 1000
+    end_timestamp_min = utils.get_end_timestamp_minute(end_timestamp)
+    print("\nstart send event until: " + end_timestamp_min + ", day user number: " + str(len(day_users)))
+
+    executor = ThreadPoolExecutor(configure.THREAD_NUMBER_FOR_USER_PERFORMANCE + 1)
+    n = int(len(day_users) / configure.THREAD_NUMBER_FOR_USER_PERFORMANCE) + 1
+    user_arr = [day_users[i:i + n] for i in range(0, len(day_users), n)]
+
+    handled_thread_count = 0
+    for users in user_arr:
+        all_events = get_user_event_of_duration(users, start_timestamp)
+        handled_thread_count += 1
+        print("started thread: " + str(handled_thread_count) + " with " + str(len(all_events)) + " users")
+        if handled_thread_count == configure.THREAD_NUMBER_FOR_USER_PERFORMANCE:
+            print("\nall events count in " + str(configure.BATCH_EVENT_DURATION_IN_MINUTES_PERFORMANCE) + " minutes: " + str(
+                global_total_events_for_duration) + ", user count: " + str(global_total_users_for_duration) + "\n")
+        executor.submit(send_user_event_of_duration, users, all_events, end_timestamp)
+    global left_users, left_events
+
+    if len(left_users) > 0:
+        for i in range(len(left_users)):
+            left_users[i].total_day_events += len(left_events[i])
+        executor.submit(send_user_event_of_duration, left_users.copy(), left_events.copy(), end_timestamp)
+        left_events = []
+        left_users = []
+    executor.shutdown(wait=True)
+
+    print("there are " + str(len(left_users)) + " users events left to send\n")
+    print("end duration events sending\n\n")
+
+
+if __name__ == '__main__':
+    configure.init_config()
+    if configure.APP_ID == "" or configure.ENDPOINT == "":
+        print("Error: please config your appId and endpoint")
+    else:
+        users = init_all_user()
+        day = ""
+        day_users = []
+        while True:
+            if day != utils.get_current_day():
+                users_count = random.choices(configure.RANDOM_DAU_PERFORMANCE)[0]
+                day_users = random.sample(users, users_count)
+                day = utils.get_current_day()
+            create_duration_event(day_users)
