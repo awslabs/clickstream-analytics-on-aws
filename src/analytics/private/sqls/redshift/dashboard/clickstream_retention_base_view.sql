6122df437bc33fcd3106c965e82832a5d74a5bfa
liwmin <liwmin@amazon.com>
2024-04-13 17:45:45 +0800

    Modify SQL to handle abnormal data

--- src/analytics/private/sqls/redshift/dashboard/clickstream_retention_base_view.sql
+++ src/analytics/private/sqls/redshift/dashboard/clickstream_retention_base_view.sql
@@ -9,6 +9,7 @@ select
   DATE_DIFF('day', DATE(CONVERT_TIMEZONE('{{{timezone}}}', TIMESTAMP 'epoch' + first_touch_time_msec/1000 * INTERVAL '1 second')), DATE(CONVERT_TIMEZONE('{{{timezone}}}', event_timestamp))) as day_diff
 from {{database_name}}.{{schema}}.{{baseView}}
 where event_timestamp >= TIMESTAMP 'epoch' + first_touch_time_msec/1000 * INTERVAL '1 second'
+and first_touch_time_msec is not null
 and day_diff <= 42 
 group by 1,2,3
 ;
--- src/analytics/private/sqls/redshift/dashboard/clickstream_retention_view_v3.sql
+++ src/analytics/private/sqls/redshift/dashboard/clickstream_retention_view_v3.sql
@@ -1,6 +1,15 @@
 CREATE OR REPLACE VIEW {{database_name}}.{{schema}}.{{viewName}}
 AS
-WITH total_users AS (
+WITH 
+except_user as (
+    select
+      user_pseudo_id
+    FROM shop.clickstream_retention_base_view_test
+    group by 1
+    having(min(day_diff)) > 0
+),
+
+total_users AS (
   SELECT
     first_date,
     COUNT(user_pseudo_id) AS total_users
@@ -8,7 +17,12 @@ WITH total_users AS (
     SELECT
       first_date,
       user_pseudo_id
-    FROM {{database_name}}.{{schema}}.clickstream_retention_base_view
+    FROM {{database_name}}.{{schema}}.clickstream_retention_base_view as view
+    WHERE NOT EXISTS (
+      SELECT 1
+      FROM except_user
+      WHERE except_user.user_pseudo_id = view.user_pseudo_id
+    )
     GROUP BY 1, 2
   ) t2
   GROUP BY 1
@@ -19,7 +33,12 @@ retention_counts AS (
     first_date,
     day_diff,
     COUNT(distinct user_pseudo_id) AS returned_user_count
-  FROM {{database_name}}.{{schema}}.clickstream_retention_base_view
+  FROM {{database_name}}.{{schema}}.clickstream_retention_base_view as view
+  WHERE NOT EXISTS (
+      SELECT 1
+      FROM except_user
+      WHERE except_user.user_pseudo_id = view.user_pseudo_id
+    )
   GROUP BY first_date, day_diff
 )
 
