b97a3537c8b9fc16e5d0ad2d134fd86dda3120c9
mingfeiq <mingfeiq@amazon.com>
2024-04-15 09:07:58 +0000

    support HongKong

--- frontend/src/ts/const.ts
+++ frontend/src/ts/const.ts
@@ -568,4 +568,4 @@ export const INIT_SEGMENT_OBJ: ExtendSegment = {
 };
 
 export const TABLE_FILTER_OPTIONS = [':', '!:', '=', '!='];
-export const FILTER_TIME_ZONE = ['Hongkong'];
+export const FILTER_TIME_ZONE = [];
