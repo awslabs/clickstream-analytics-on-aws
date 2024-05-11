c33b801a7186aee57444a4f87a81d1c6eada0845
Yong Liu <yonmzn@amazon.com>
2024-05-11 16:00:55 +0800

    chore: ETL add metrics filtered by appIds, freshness, future, bot (#1262)

--- src/data-pipeline/spark-etl/src/main/java/software/aws/solution/clickstream/UAEnrichmentV2.java
+++ src/data-pipeline/spark-etl/src/main/java/software/aws/solution/clickstream/UAEnrichmentV2.java
@@ -81,11 +81,13 @@ public class UAEnrichmentV2 {
         }
         Dataset<Row> enrichedDatasetFiltered = enrichedDataset;
         String filterBotByUAStr = System.getProperty(FILTER_BOT_BY_UA_PROP);
-        if (Boolean.parseBoolean(filterBotByUAStr)) {
+        if (filterBotByUAStr == null || Boolean.parseBoolean(filterBotByUAStr)) {
+            long beforeFilterCount = enrichedDataset.count();
             enrichedDatasetFiltered = enrichedDataset.filter(
                     col(Constant.DEVICE_UA_DEVICE_CATEGORY).notEqual(UAEnrichHelper.BOT)
             );
-            log.info("UA Enriched enrichedDatasetFiltered(BOT) count: {}", enrichedDatasetFiltered.count());
+            long afterFilterCount = enrichedDatasetFiltered.count();
+            log.info(new ETLMetric(beforeFilterCount - afterFilterCount, "filtered by Bot").toString());
         }
         return enrichedDatasetFiltered;
     }
