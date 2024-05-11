3770986bbe540a27be7133973fe3106146946dd2
llmin <liwmin@amazon.com>
2024-05-11 16:26:05 +0800

    fix: event base view miss data (#1265)

--- src/analytics/private/sqls/redshift/dashboard/clickstream_acquisition_day_user_view_cnt_sp.sql
+++ src/analytics/private/sqls/redshift/dashboard/clickstream_acquisition_day_user_view_cnt_sp.sql
@@ -8,7 +8,7 @@ BEGIN
   current_date := day;
 
   WHILE i < ndays LOOP
-    DELETE FROM {{database_name}}.{{schema}}.{{viewName}} where event_date = day;
+    DELETE FROM {{database_name}}.{{schema}}.{{viewName}} where event_date = current_date;
     
     INSERT INTO {{database_name}}.{{schema}}.{{viewName}} (
       event_date, 
@@ -18,14 +18,14 @@ BEGIN
       view_count
     )
     SELECT 
-      day::date AS event_date,
+      current_date::date AS event_date,
       platform,
       merged_user_id as "Active users", 
       SUM(CASE WHEN event_name = '_first_open' THEN 1 ELSE 0 END) AS "New users",
       SUM(CASE WHEN event_name = '_screen_view' OR event_name = '_page_view' THEN 1 ELSE 0 END) AS view_count
     FROM 
       {{database_name}}.{{schema}}.{{baseView}}
-    where DATE_TRUNC('day', CONVERT_TIMEZONE(timezone, event_timestamp)) = day
+    where DATE_TRUNC('day', CONVERT_TIMEZONE(timezone, event_timestamp)) = current_date
     GROUP BY 
       1,2,3
     ;
