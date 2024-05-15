0769e3e7b3e9a1f36900e321e7192087dd4766ee
liwmin <liwmin@amazon.com>
2024-05-15 16:48:37 +0800

    change max length of log table columns

--- src/analytics/private/sqls/redshift/clickstream-log.sql
+++ src/analytics/private/sqls/redshift/clickstream-log.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS {{schema}}.{{table_clickstream_log}} (
     id varchar(256),
-    log_name varchar(50),
-    log_level varchar(10), 
-    log_msg varchar(256),
+    log_name varchar(256),
+    log_level varchar(32), 
+    log_msg varchar(65535),
     log_date TIMESTAMP default getdate()
 );
