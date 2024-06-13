71bbba33a04973fd0e9d4447d0dbe56558b90572
quemingfei <mingfeiq@amazon.com>
2024-06-13 21:17:46 +0800

    fix: project edit error and allow DevMode change (#1452)

--- src/control-plane/backend/lambda/api/model/stacks.ts
+++ src/control-plane/backend/lambda/api/model/stacks.ts
@@ -135,6 +135,7 @@ export class CIngestionServerStack extends JSONObject {
       'EnableApplicationLoadBalancerAccessLog',
       'Protocol',
       'SinkType',
+      'DevMode',
     ];
     return allowedList;
   }
