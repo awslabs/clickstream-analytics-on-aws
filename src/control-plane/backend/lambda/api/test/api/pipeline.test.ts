8491433143134d8a25bb7ffb74ff188dfec5d520
mingfeiq <mingfeiq@amazon.com>
2024-07-22 06:50:18 +0000

    fix test case

--- src/control-plane/backend/lambda/api/test/api/pipeline.test.ts
+++ src/control-plane/backend/lambda/api/test/api/pipeline.test.ts
@@ -1175,6 +1175,34 @@ describe('Pipeline test', () => {
     expect(ec2Mock).toHaveReceivedCommandTimes(DescribeRouteTablesCommand, 1);
     expect(ec2Mock).toHaveReceivedCommandTimes(DescribeVpcEndpointsCommand, 1);
   });
+  it('Create pipeline in the isolated subnets miss s3 vpc endpoint - GCR', async () => {
+    tokenMock(ddbMock, false);
+    projectExistedMock(ddbMock, true);
+    dictionaryMock(ddbMock);
+    createPipelineMock(mockClients, {
+      publicAZContainPrivateAZ: true,
+      subnetsCross3AZ: true,
+      subnetsIsolated: true,
+      missVpcEndpoint: true,
+      bucket: {
+        location: BucketLocationConstraint.cn_north_1,
+      },
+    });
+    ddbMock.on(PutCommand).resolves({});
+    const res = await request(app)
+      .post('/api/pipeline')
+      .set('X-Click-Stream-Request-Id', MOCK_TOKEN)
+      .send({
+        ...KINESIS_DATA_PROCESSING_NEW_REDSHIFT_PIPELINE,
+        region: 'cn-north-1',
+      });
+    expect(res.headers['content-type']).toEqual('application/json; charset=utf-8');
+    expect(res.statusCode).toBe(400);
+    expect(res.body.message).toEqual('Validation error: vpc endpoint error in subnet: subnet-00000000000000011, detail: [{\"service\":\"cn.com.amazonaws.cn-north-1.s3\",\"reason\":\"Miss vpc endpoint\"}].');
+    expect(ec2Mock).toHaveReceivedCommandTimes(DescribeSubnetsCommand, 1);
+    expect(ec2Mock).toHaveReceivedCommandTimes(DescribeRouteTablesCommand, 1);
+    expect(ec2Mock).toHaveReceivedCommandTimes(DescribeVpcEndpointsCommand, 1);
+  });
   it('Create pipeline with provisioned redshift empty dbuser', async () => {
     tokenMock(ddbMock, false);
     projectExistedMock(ddbMock, true);
